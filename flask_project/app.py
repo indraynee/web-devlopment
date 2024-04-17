06720d92edb5039caa01b5d9b5590fafdb236646
unknown <harshal.nelge98@gmail.com>
2024-04-18 01:27:16 +0530

    .

--- flask_project/app.py
+++ flask_project/app.py
@@ -101,7 +101,6 @@ def signup():
             return redirect(url_for('landing'))  # Redirect to homepage after successful signup
     # Pass the errors dictionary to the template
     return render_template('sign_up.html', errors=errors)
- .
 @app.route('/create_blog.html', methods=['GET', 'POST'])
 def create_blog():
     if request.method == 'POST':
